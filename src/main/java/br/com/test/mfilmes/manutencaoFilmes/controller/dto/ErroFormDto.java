3259dcca66fb62e47d1738dfccee40525c380948
Mariana Valentim <marihvalentin_2@hotmail.com>
2020-08-11 19:53:34 -0300

    Classe que complementa a implementação da ErroValidacaoHandler

--- src/main/java/br/com/test/mfilmes/manutencaoFilmes/controller/dto/ErroFormDto.java
+++ src/main/java/br/com/test/mfilmes/manutencaoFilmes/controller/dto/ErroFormDto.java
@@ -1,4 +1,23 @@
 package br.com.test.mfilmes.manutencaoFilmes.controller.dto;
 
-public class ErroFormDto {
+public class ErroFormDto
+{
+    private String campoErro;
+    private String mensagemErro;
+
+    public ErroFormDto(String campoErro, String menssagemErro)
+    {
+        this.campoErro = campoErro;
+        this.mensagemErro = menssagemErro;
+    }
+
+    public String getCampoErro()
+    {
+        return campoErro;
+    }
+
+    public String getMensagemErro()
+    {
+        return mensagemErro;
+    }
 }
