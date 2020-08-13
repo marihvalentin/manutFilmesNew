10aaf2444511f48616a1cac437ee22e8b16c08c2
Mariana Valentim <marihvalentin_2@hotmail.com>
2020-08-13 15:48:56 -0300

    adicionados novos dados de inserts para tabela filmes e realizada ordenação por lançamento

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -18,9 +18,13 @@ insert into genero (tipo) values ('Suspense');
 insert into genero (tipo) values ('Terror');
 insert into genero (tipo) values ('Thriller');
 
-insert into filme (titulo, diretor, genero_id_gen, sinopse, ano) values ('Os Vingadores', 'Joss Whedon', 12,
-'Loki, o irmão de Thor, ganha acesso ao poder ilimitado  do cubo cósmico ao roubá-lo de dentro das instalações da S.H.I.E.L.D. Nick Fury, logo reúne os únicos super-heróis que serão capazes de defender a Terra de ameaças sem precedentes.', 2012);
-insert into filme (titulo, diretor, genero_id_gen, sinopse, ano) values ('Os Vingadores: Era de Ultron', 'Joss Whedon', 12,
-'Tony Stark constrói um sistema de IA que cuidaria da paz mundial. O projeto dá errado e gera o Ultron. Com o destino da Terra em jogo, Cap América, Ironman, Thor, Hulk, BlackWidow e Gavião Arqueiro terão que se unir para salvar a raça humana da extinção.', 2015);
+
+insert into filme (titulo, diretor, genero_id_gen, sinopse, ano) values ('El Chanfle', 'Enrique Segoviano', 5,
+'Chanfle é o roupeiro do América do México e coloca em prática, dentro da equipe e fora dela, sua honradez, até mesmo contra as ordens do técnico Reyes. Sua esposa Terê, sofre com o sonho de ter um filho mesmo após quase 10 anos de casados, mas com a ajuda do Dr. Nájera vão tentar conquistar seu primeiro filho antes da final do campeonato.', 1979);
 insert into filme (titulo, diretor, genero_id_gen, sinopse, ano) values ('Procurando Nemo', 'Andrew Stanton', 2,
 'Em seu primeiro dia de aula, esquecendo os conselhos do pai superprotetor, Nemo é capturado por um mergulhador e acaba no aquário de um dentista. Enquanto Nemo tenta bolar um plano para escapar, seu pai cruza o oceano para resgatá-lo.', 2003);
+insert into filme (titulo, diretor, genero_id_gen, sinopse, ano) values ('Os Vingadores', 'Joss Whedon', 12,
+'Loki, o irmão de Thor, ganha acesso ao poder ilimitado  do cubo cósmico ao roubá-lo de dentro das instalações da S.H.I.E.L.D. Nick Fury, logo reúne os únicos super-heróis que serão capazes de defender a Terra de ameaças sem precedentes.', 2012);
+insert into filme (titulo, diretor, genero_id_gen, sinopse, ano) values ('Até o Último Homem', 'Mel Gibson', 8,
+'Acompanhe a história de Desmond T. Doss, um médico do exército americano que, durante a Segunda Guerra Mundial, se recusa a pegar em armas. Durante a Batalha de Okinawa ele trabalha na ala médica e salva cerca de 75 homens.', 2016);
+
